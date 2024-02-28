eabcf16c1043939cc5453fd1ab7eb37a43ce6bae
Leics <1503647333@qq.com>
2024-02-28 15:59:15 +0800

    1.12 second

--- src/1/1.12 序列中出现次数最多的元素/test.py
+++ src/1/1.12 序列中出现次数最多的元素/test.py
@@ -5,3 +5,13 @@ words = [
    'my', 'eyes', "you're", 'under'
 ]
 
+from collections import Counter
+word_counts = Counter(words)
+# 出现频率最高的3个单词
+top_three = word_counts.most_common(3)
+print(top_three)
+
+morewords = ['why','are','you','not','looking','in','my','eyes']
+word_counts.update(morewords)
+top_three = word_counts.most_common(3)
+print(top_three)
